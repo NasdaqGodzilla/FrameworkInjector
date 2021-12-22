d4cdfd5e20fc92a668a4ce0838a33cd52dcad614
NasdaqGodzilla <26323326+NasdaqGodzilla@users.noreply.github.com>
2021-12-22 16:18:20 +0800

    [init] 创建工程结构，配置编译bp，实现主方法。

---
+++ src/fakeinjector/Utils.java
@@ -0,0 +1,55 @@
+/**
+ *
+      ___         ___           ___           ___           ___
+     /  /\       /  /\         /  /\         /  /\         /  /\
+    /  /::\     /  /:/_       /  /::\       /  /:/        /  /:/_
+   /  /:/\:\   /  /:/ /\     /  /:/\:\     /  /:/        /  /:/ /\
+  /  /:/ /:/  /  /:/ /:/_   /  /:/ /::\   /  /:/  ___   /  /:/ /:/_
+ /__/:/ /:/  /__/:/ /:/ /\ /__/:/ /:/\:\ /__/:/  /  /\ /__/:/ /:/ /\
+ \  \:\/:/   \  \:\/:/ /:/ \  \:\/:/__\/ \  \:\ /  /:/ \  \:\/:/ /:/
+  \  \::/     \  \::/ /:/   \  \::/       \  \:\  /:/   \  \::/ /:/
+   \  \:\      \  \:\/:/     \  \:\        \  \:\/:/     \  \:\/:/
+    \  \:\      \  \::/       \  \:\        \  \::/       \  \::/
+     \__\/       \__\/         \__\/         \__\/         \__\/
+      ___           ___           ___           ___           ___
+     /__/\         /  /\         /__/|         /  /\         /  /\
+    |  |::\       /  /::\       |  |:|        /  /:/_       /  /::\
+    |  |:|:\     /  /:/\:\      |  |:|       /  /:/ /\     /  /:/\:\
+  __|__|:|\:\   /  /:/ /::\   __|  |:|      /  /:/ /:/_   /  /:/ /:/
+ /__/::::| \:\ /__/:/ /:/\:\ /__/\_|:|____ /__/:/ /:/ /\ /__/:/ /:/___
+ \  \:\~~\__\/ \  \:\/:/__\/ \  \:\/:::::/ \  \:\/:/ /:/ \  \:\/:::::/
+  \  \:\        \  \::/       \  \::/~~~~   \  \::/ /:/   \  \::/~~~~
+   \  \:\        \  \:\        \  \:\        \  \:\/:/     \  \:\
+    \  \:\        \  \:\        \  \:\        \  \::/       \  \:\
+     \__\/         \__\/         \__\/         \__\/         \__\/
+ *
+ * Filename     @ Utils.java
+ * Create date  @ 2021-12-22 15:47:51
+ * Description  @
+ * version      @ V1.0.0
+ */
+
+package peacemaker.frameworkinjector;
+
+public class Utils {
+
+    public static final String LOG_TAG = "frameworkinjector";
+
+    public static void message(String tag, String msg) {
+        assert msg != null;
+
+        final java.lang.StringBuilder sb = new StringBuilder("");
+        sb.append("" + tag + ":\t");
+        sb.append(msg);
+        System.out.println(sb.toString());
+    }
+
+    public static void fatal(String tag, String msg) throws FatalMessageException {
+        message(tag, msg);
+
+        throw new FatalMessageException("Stop with fatal: " + tag + " " + msg);
+    }
+
+    public class FatalMessageException extends IllegalStateException {}
+}
+
