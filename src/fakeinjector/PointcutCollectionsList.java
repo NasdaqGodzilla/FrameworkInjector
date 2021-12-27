945aac8104ed68b75ec2c4a3a952f3452e07623c
NasdaqGodzilla <26323326+NasdaqGodzilla@users.noreply.github.com>
2021-12-27 15:10:26 +0800

    [feature][PointcutCollectionsList] 设计、实现PointcutCollectionsList。

--- src/fakeinjector/PointcutCollectionsList.java
+++ src/fakeinjector/PointcutCollectionsList.java
@@ -37,10 +37,46 @@ public class PointcutCollectionsList extends java.util.LinkedList<BasePointcutCo
     }
 
     public boolean containsStyledIdentifier(CharSequence identifier) {
+        assert !Utils.isEmpty(identifier);
+
         for (BasePointcutCollections e : this)
             if (Utils.equals(identifier, e.getStyledIdentifier()))
                 return true;
         return false;
+    }
+
+    public BasePointcutCollections get(CharSequence identifier) {
+        assert !Utils.isEmpty(identifier);
+
+        for (BasePointcutCollections e : this)
+            if (Utils.equals(identifier, e.getStyledIdentifier()))
+                return e;
+
+        return null;
+    }
+
+    public BasePointcutCollections getOrCreate(CharSequence identifier) {
+        if (containsStyledIdentifier(identifier))
+            return get(identifier);
+
+        BasePointcutCollections e = new ClassPointcutCollections(identifier);
+        add(e);
+        return e;
+    }
+
+    public BasePointcutCollections add(CharSequence styledIdentifier) {
+        assert !Utils.isEmpty(styledIdentifier);
+
+        return getOrCreate(styledIdentifier);
+    }
+
+    public BasePointcutCollections add(BaseMethodPointcut pointcut) {
+        assert null != pointcut;
+
+        ClassPointcutCollections e =
+            (ClassPointcutCollections) getOrCreate(pointcut.getStyledIdentifier());
+        e.ate(pointcut);
+        return e;
     }
 }
 
