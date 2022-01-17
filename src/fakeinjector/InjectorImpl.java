3f17788a72842d7ef0c20344251f710fd728f8e6
NasdaqGodzilla <26323326+NasdaqGodzilla@users.noreply.github.com>
2022-01-17 10:58:32 +0800

    [feature][InjectorImpl] 设计实现InjectorImpl.

---
+++ src/fakeinjector/InjectorImpl.java
@@ -0,0 +1,60 @@
+/**
+ *
+      ___         ___           ___           ___           ___
+     /  /\       /  /\         /  /\         /  /\         /  /\
+    /  /::\     /  /:/_       /  /::\       /  /:/        /  /:/_
+   /  /:/\:\   /  /:/ /\     /  /:/\:\     /  /:/        /  /:/ /\
+  /  /:/ /:/  /  /:/ /:/_   /  /:/ /::\   /  /:/  ___   /  /:/ /:/_
+ /__/:/ /:/  /__/:/ /:/ /\ /__/:/ /:/\:\ /__/:/  /  /\ /__/:/ /:/ /\
+ \  \:\/:/   \  \:\/:/ /:/ \  \:\/:/__\/ \  \:\ /  /:/ \  \:\/:/ /:/
+  \  \::/     \  \::/ /:/   \  \::/       \  \:\  /:/   \  \::/ /:/
+   \  \:\      \  \:\/:/     \  \:\        \  \:\/:/     \  \:\/:/
+    \  \:\      \  \::/       \  \:\        \  \::/       \  \::/
+     \__\/       \__\/         \__\/         \__\/         \__\/
+      ___           ___           ___           ___           ___
+     /__/\         /  /\         /__/|         /  /\         /  /\
+    |  |::\       /  /::\       |  |:|        /  /:/_       /  /::\
+    |  |:|:\     /  /:/\:\      |  |:|       /  /:/ /\     /  /:/\:\
+  __|__|:|\:\   /  /:/ /::\   __|  |:|      /  /:/ /:/_   /  /:/ /:/
+ /__/::::| \:\ /__/:/ /:/\:\ /__/\_|:|____ /__/:/ /:/ /\ /__/:/ /:/___
+ \  \:\~~\__\/ \  \:\/:/__\/ \  \:\/:::::/ \  \:\/:/ /:/ \  \:\/:::::/
+  \  \:\        \  \::/       \  \::/~~~~   \  \::/ /:/   \  \::/~~~~
+   \  \:\        \  \:\        \  \:\        \  \:\/:/     \  \:\
+    \  \:\        \  \:\        \  \:\        \  \::/       \  \:\
+     \__\/         \__\/         \__\/         \__\/         \__\/
+ *
+ * Filename     @ Main.java
+ * Filename     @ InjectorImpl.java
+ * Create date  @ 2022-01-17 10:40:58
+ * Description  @
+ * version      @ V1.0.0
+ */
+
+package peacemaker.frameworkinjector;
+
+import javassist.ClassPool;
+
+class InjectorImpl implements AutoCloseable {
+    private static final ClassPool sClassPool;
+
+    public static InjectorImpl get() {
+        return Instance.i;
+    }
+
+    public static ClassPool retrieveClassPool() {
+        return sClassPool;
+    }
+
+    private InjectorImpl() {}
+
+    private static class Instance {
+        private static final InjectorImpl i = new InjectorImpl();
+        InjectorImpl.sClassPool = ClassPool.getDefault();
+    }
+
+    @Override
+    public void close() {
+        sClassPool = null;
+    }
+}
+
