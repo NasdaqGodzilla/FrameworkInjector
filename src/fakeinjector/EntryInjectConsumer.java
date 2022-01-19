b370d4ff1d20df2b8b4d9bd613fb0c173c0634a2
NasdaqGodzilla <26323326+NasdaqGodzilla@users.noreply.github.com>
2022-01-19 15:48:26 +0800

    [feature][EntryInjectConsumer] 设计实现EntryInjectConsumer的注入功能。

--- src/fakeinjector/EntryInjectConsumer.java
+++ src/fakeinjector/EntryInjectConsumer.java
@@ -31,7 +31,7 @@
 
 package peacemaker.frameworkinjector;
 
-import java.io.BufferedInputStream;
+import java.io.InputStream;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.zip.ZipEntry;
@@ -42,44 +42,84 @@ class EntryInjectConsumer extends EntryCopyConsumer {
 
     protected WeakReference<PointcutCollectionsList> mWeakList = null;
 
-    // 计量注入次数
-    private int mCntTheInject;
-
     public EntryInjectConsumer(ZipOutputStream zos) {
         super(zos);
 
         mWeakList =
             new WeakReference<>(InjectEngine.get().retrievePointcutCollectionsList());
-        mCntTheInject = 0;
     }
 
-    private boolean matched(String s) {
+    // 验证PointcutCollectionsList包含对应该Entry的切点
+    private boolean matched(String s /* ZipEntry.getName() */) {
         return EntryMatchFilter.matched(s, mWeakList.get());
     }
 
-    // For t/test: getPoints().forEach()
-    protected BasePointcutCollections retrievePointcuts(String clzName) {
+    protected BasePointcutCollections retrievePointcuts(String entryName) {
+        return mWeakList.get().get(toNameWithPoint(entryName));
+    }
+
+    static String toNameWithPoint(String entryName) {
         final SpecFormatter sf =
-            SpecFormatter.with(SpecFormatter.retrieveEntryNameWithoutSuffix(clzName));
+            SpecFormatter.with(SpecFormatter.retrieveEntryNameWithoutSuffix(entryName));
         final String classFullNamePoint =
             sf.retrieveInnerSpecAs(sf.retrieveWithPoint(), ".");
 
-        return mWeakList.get().get(classFullNamePoint);
+        return classFullNamePoint;
     }
 
-    /*
     @Override
-    public void accept(ZipEntry newEntry, BufferedInputStream newEntryBis) {
+    public void accept(ZipEntry newEntry, InputStream newEntryBis) {
+        final String newEntryName = newEntry.getName().trim();
+        final String clzNameWithPoint = toNameWithPoint(newEntryName);
 
+        if (!matched(newEntryName)) {
+            message("Not accept due to unmatched: " + newEntryName);
+            super.accept(newEntry, newEntryBis);
+            return;
+        }
+
+        final BasePointcutCollections pointcuts = retrievePointcuts(newEntryName);
+        if (null == pointcuts || 0 == pointcuts.getPoints().size()) {
+            fatal("Trying accept matched entry but failed to retrieve pointcuts. " +
+                    newEntryName);
+            return;
+        }
+
+        try (final InjectorImpl injector = InjectorImpl.get();
+            final CtClassWrapper ctClassWrapper = InjectorImpl.ClazzLoader
+                .makeClass(injector.retrieveClassPool(), newEntryBis, clzNameWithPoint);) {
+            // TODO: Assert CtClassWrapper class name equals with clzNameWithPoint;
+            final String loadedClassName = ctClassWrapper.getCtName();
+            message("Accept: ClazzLoader success load " + loadedClassName);
+
+            final java.util.List<? extends InjectorImpl.InjectTarget> injectTargets =
+                InjectorImpl.InjectTarget.with(ctClassWrapper, pointcuts.getPoints());
+            if (0 == injectTargets.size())
+                fatal("Trying accept matched and loaded class but failed to retrieve inject targets. " +
+                        " Pointcuts size: " + pointcuts.getPoints().size());
+
+            injectTargets.forEach(InjectorImpl.Translator::performTargetInject);
+
+            boolean flag = false;
+            try (final java.io.ByteArrayInputStream bais =
+                    new java.io.ByteArrayInputStream(ctClassWrapper.retrieveCtClass().toBytecode());) {
+                super.accept(newEntry, bais);
+                flag = !flag;
+            } catch (javassist.CannotCompileException | IOException e) {
+                fatal("Accept: Cannot generate injected class. " + e);
+            } finally {
+                message(String.format("Accept: finally finished: %s [%s]",
+                            clzNameWithPoint, flag ? "SUCCESS" : "FAILED"));
+            }
+
+        } catch (Utils.FatalMessageException e) {
+            fatal("Accept: fatal error... " + e);
+        }
     }
-    */
 
     @Override
     public void close() {
         super.close();
-
-        message(String.format("Finally done... mCntTheInject[%d]",
-                    mCntTheInject));
     }
 
     private static final void message(CharSequence cs) {
